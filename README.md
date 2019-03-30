a621468c5487c93a1dd86b84db0fe3babeddc6a2
Paulo Lima <paulo.phlp@outlook.com>
2019-03-29 21:04:00 -0300

    Starting to add Node.Js into the project.

--- README.md
+++
@@ -1,4 +0,0 @@
-# NodeJs - Awesome Pics Project
-An app to post and see awesome pics made with Node.Js.
-
-In this project I've used HTML, CSS, JavaScript and Node.Js.
---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const moment = require('moment');
+const config = require('./config/config').get(process.env.NODE_ENV);
+const {User} = require('./models/user');
+const {Auth} = require('./middleware/auth');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE, {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + './../views/layouts',
+    partialsDir: __dirname + './../views/partials'
+}));
+app.set('view engine', 'hbs');
+app.use('/css', express.static(__dirname + './../public/css'));
+app.use('/js', express.static(__dirname + './../public/js'));
+app.use(bodyParser.json());
+// app.use(cookieParser);
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(config.PORT, () => {
+    console.log(`Awesome Pics running on port ${config.PORT}.`);
+})
